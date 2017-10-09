f568fc634c18621c6a3158729f6e16f90b6a9a3b
Andrea Cotza <andrea@cotza.me>
2017-10-09 14:09:25 +0200

    Add picture (/sdcard/Download/perso.jpg)

--- app/src/main/java/ch/heigvd/sym/template/InfoActivity.java
+++ app/src/main/java/ch/heigvd/sym/template/InfoActivity.java
@@ -1,19 +1,25 @@
 package ch.heigvd.sym.template;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.TelephonyManager;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class InfoActivity extends AppCompatActivity {
 
     private TextView email = null;
     private TextView imei = null;
+    private ImageView persoImg = null;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -22,13 +28,21 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         this.email = (TextView) findViewById(R.id.email);
+        this.imei = (TextView) findViewById(R.id.imei);
+        this.persoImg = (ImageView) findViewById(R.id.persoImg);
+
         String userEmail = getIntent().getStringExtra("USER_EMAIL");
         email.setText(userEmail);
 
-        this.imei = (TextView) findViewById(R.id.imei);
         TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         String userImei = telephonyManager.getImei();
         imei.setText(userImei);
+
+        File imgPath =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File imgFile = new File(imgPath, "perso.jpg");
+        if(imgFile.exists()) {
+            persoImg.setImageBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath()));
+        }
 
     }
 

3cff33279e55222006ed43fec99ebb4a9aa29c0f
Andrea Cotza <andrea@cotza.me>
2017-10-09 16:01:54 +0200

    Request permission the right way

--- app/src/main/java/ch/heigvd/sym/template/InfoActivity.java
+++ app/src/main/java/ch/heigvd/sym/template/InfoActivity.java
@@ -1,18 +1,18 @@
 package ch.heigvd.sym.template;
 
+import android.Manifest;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.telephony.TelephonyManager;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.greysonparrelli.permiso.Permiso;
 import java.io.File;
 
 public class InfoActivity extends AppCompatActivity {
@@ -25,25 +25,53 @@ public class InfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Permiso.getInstance().setActivity(this);
         setContentView(R.layout.activity_info);
 
         this.email = (TextView) findViewById(R.id.email);
         this.imei = (TextView) findViewById(R.id.imei);
         this.persoImg = (ImageView) findViewById(R.id.persoImg);
 
+        Permiso.getInstance().requestPermissions(new Permiso.IOnPermissionResult() {
+            @Override
+            public void onPermissionResult(Permiso.ResultSet resultSet) {
+                if (resultSet.isPermissionGranted(Manifest.permission.READ_PHONE_STATE)) {
+                    TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                    String userImei = telephonyManager.getImei();
+                    imei.setText(userImei);
+                }
+                else {
+                    imei.setText("Permission denied");
+                }
+                if (resultSet.isPermissionGranted(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    File imgPath =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                    File imgFile = new File(imgPath, "perso.jpg");
+                    if(imgFile.exists()) {
+                        persoImg.setImageBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath()));
+                    }
+                }
+            }
+
+            @Override
+            public void onRationaleRequested(Permiso.IOnRationaleProvided callback, String... permissions) {
+                Permiso.getInstance().showRationaleInDialog("Permission denied", "Without the following permissions we can't show the IMEI and/or the user picture", null, callback);
+            }
+        }, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE);
+
         String userEmail = getIntent().getStringExtra("USER_EMAIL");
         email.setText(userEmail);
+    }
 
-        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        String userImei = telephonyManager.getImei();
-        imei.setText(userImei);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Permiso.getInstance().setActivity(this);
+    }
 
-        File imgPath =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        File imgFile = new File(imgPath, "perso.jpg");
-        if(imgFile.exists()) {
-            persoImg.setImageBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath()));
-        }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Permiso.getInstance().onRequestPermissionResult(requestCode, permissions, grantResults);
     }
 
 }
